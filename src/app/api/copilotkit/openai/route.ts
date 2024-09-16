0350951452e0a75fe73efc0b25e660258f2296df
Arhan Ansari <arhanansari2009@gmail.com>
2024-09-16 18:20:31 +0530

    Update route.ts

--- src/app/api/copilotkit/openai/route.ts
+++ src/app/api/copilotkit/openai/route.ts
@@ -1,9 +1,9 @@
-import { CopilotBackend, OpenAIAdapter } from "@copilotkit/backend";
+import { CopilotBackend, GoogleGenerativeAIAdapter } from "@copilotkit/backend";
 
 export const runtime = "edge";
 
 export async function POST(req: Request): Promise<Response> {
   const copilotKit = new CopilotBackend();
 
-  return copilotKit.response(req, new OpenAIAdapter({model:"gpt-4"}));
+  return copilotKit.response(req, new GoogleGenerativeAIAdapter({model:"gemini-pro"}));
 }
